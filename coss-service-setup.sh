23cd9667e37b917482f3b60a32192dc064b0b673
electrob <aman.kashyap47@gmail.com>
2021-02-09 16:57:07 +0530

    added comments and descriptions

--- coss-service-setup.sh
+++ coss-service-setup.sh
@@ -1,39 +1,51 @@
 #!/usr/bin/env bash
 
 user=jet
+
+# Specific to Jetson Nano
+# Defaulting to 5Watts mode  
 sudo nvpmodel -m 1
+
+# Constant journal logs for all the services combined
 sudo journalctl --vacuum-size=500M
 
-cd /home/"${user}"/Desktop/catalystos-surveillance/
+cd /home/"${user}"/Desktop/rpi-surveillance/
 
-sudo systemctl stop catalystos-surveillance.service
+# Stopping and disabling the related services
+sudo systemctl stop rpi-surveillance.service
 sudo systemctl stop nvargus-daemon.service
-sudo systemctl disable catalystos-surveillance.service
+sudo systemctl disable rpi-surveillance.service
 sudo rm /etc/systemd/system/nvargus-daemon.service
-sudo rm /lib/systemd/system/catalystos-surveillance.service
+sudo rm /lib/systemd/system/rpi-surveillance.service
 sudo systemctl reset-failed
 
 sudo cp ./nvargus-daemon.service /etc/systemd/system/
 
+# Defining the service UNIT file 
 echo "[Unit]
-Description=CatalystOS Surveillance Service
-StartLimitIntervalSec=900
-StartLimitAction=reboot
+Description=RPI Surveillance Service
+
+# This is for restarting the whole device if the software doesnt work
+# Uncomment if you need this kind of feature 
+# SartLimitIntervalSec=900
+# StartLimitAction=reboot
+
 After=network.target
 Requires=nvargus-daemon.service
 BindsTo=nvargus-daemon.service
 
 [Service]
 Type=simple
-ExecStart=/bin/sh /home/"${user}"/catalystos-survellance/catalystos_surveillance.sh
+ExecStart=/bin/sh /home/"${user}"/rpi-surveillance/rpi-surveillance.sh
 restart
 User="${user}"
 
 [Install]
-WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/catalystos.service
+WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/rpi-surveillance.service
 
+# Describing the UNIT file for the updation script
 echo "[Unit]
-Description=CatalystOS Surveillance Service Update
+Description=RPI Surveillance Service Update
 After=network.target
 
 [Service]
@@ -41,13 +53,14 @@ Type=simple
 User="${user}"
 Restart=always
 RestartSec=600
-ExecStart=/bin/sh -c /home/"${user}"/catalystos-surveillance/catalystos-surveillance-update.sh
+ExecStart=/bin/sh -c /home/"${user}"/catalystos-surveillance/rpi-surveillance-update.sh
 
 [Install]
-WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/catalystos-surveillance-update.service
+WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/rpi-surveillance-update.service
 
+# Reloading the linux service daemon and restarting the related services
 sudo systemctl daemon-reload
 
-sudo systemctl enable catalystos-surveillance.service
-sudo systemctl start catalystos-surveillance.service
+sudo systemctl enable rpi-surveillance.service
+sudo systemctl start rpi-surveillance.service
 
